853970cbec902079e37c7ccbe3920d116cd3ba03
markforest <toxerius7@gmail.com>
2018-09-05 19:06:15 +0300

    commit 1

---
+++ WindowsFormsApp1/DataAdapterDemo/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace DataAdapterDemo
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn = null;
+        SqlDataAdapter da = null;
+        DataSet set = null;
+        SqlCommandBuilder builder = null;
+        string cs = "";
+        public Form1()
+        {
+            InitializeComponent();
+            cs = ConfigurationManager.ConnectionStrings["My"].ConnectionString;
+            conn = new SqlConnection(cs);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sql = textBox1.Text;
+                da = new SqlDataAdapter(sql, conn);
+                dataGridView1.DataSource = null;
+                builder = new SqlCommandBuilder(da);
+                set = new DataSet();
+                da.Fill(set, "MyTable");
+                dataGridView1.DataSource = set.Tables["MyTable"];
+            }
+            catch { }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            da.Update(set, "MyTable");
+        }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private SqlDataReader reader;
+        DataTable table;
+        SqlConnection conn;
+        string cs = "";
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new SqlConnection();
+            string cs = ConfigurationManager.ConnectionStrings["My"].ConnectionString;
+            conn.ConnectionString = cs;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                SqlCommand comm = new SqlCommand();
+                comm.CommandText = textBox1.Text;
+                comm.Connection = conn;
+                dataGridView1.DataSource = null;
+                table = new DataTable();
+                reader = comm.ExecuteReader();
+                int line = 0;
+                while (reader.Read())
+                {
+                    if(line == 0)
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            table.Columns.Add(reader.GetName(i));
+                        }
+                        line++;
+                    }
+
+                    DataRow row = table.NewRow();
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        row[i] = reader[i];
+                    }
+                    table.Rows.Add(row);
+
+                }
+                dataGridView1.DataSource = table;
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
